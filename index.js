1831ecf686b58e3068af2a8bc7145f8760dacea6
Glitch (taiwa) <none>
2023-12-13 02:57:30 +0000

    Import of tenyu0723/glitch-v13-temp

---
+++ index.js
@@ -0,0 +1,59 @@
+const http = require('http')
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { Pagination } = require("discordjs-button-embed-pagination");
+const moment = require('moment');
+const Keyv = require('keyv');
+const client = new Client({
+  partials: ["CHANNEL"],
+  intents: new Intents(32767),
+  restTimeOffset: -1000
+});
+const newbutton = (buttondata) => {
+  return {
+    components: buttondata.map((data) => {
+      return {
+        custom_id: data.id,
+        label: data.label,
+        style: data.style || 1,
+        url: data.url,
+        emoji: data.emoji,
+        disabled: data.disabled,
+        type: 2,
+      };
+    }),
+    type: 1,
+  };
+};
+const prefix = ""
+const cmd_list = []
+const json = require("./command.json")
+process.env.TZ = 'Asia/Tokyo'
+
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' })
+    response.end(`${client.user.tag} is ready!\n導入サーバー:${client.guilds.cache.size}\nユーザー:${client.users.cache.size}`)
+  })
+  .listen(3000)
+
+if (process.env.DISCORD_BOT_TOKEN == undefined) {
+  console.error('tokenが設定されていません！')
+  process.exit(0)
+}
+
+client.on('ready', async () => {
+  const you = client.users.cache.get(ID)
+    client.user.setActivity(`${client.user.username} | prefix=${prefix} | 製作者:${you.tag} | ${client.guilds.cache.size}servers | ${client.users.cache.size}members`, {
+      type: 'PLAYING'
+    });
+    client.user.setStatus("idle");
+  console.log(`${client.user.tag} is ready!`);
+  const embed = new MessageEmbed()
+  .setTitle("起動しました！")
+  .setDescription(">>> ```diff\n+ Hello World!　　　　　``````diff\n+ 導入サーバー数:" + client.guilds.cache.size + "\n+ ユーザー数:" + client.users.cache.size + "```" + moment().format("YYYY-MM-DD HH:mm:ss"))
+  .setThumbnail(client.user.displayAvatarURL())
+  .setColor("RANDOM")
+  client.channels.cache.get(ID).send({ embeds:[embed] })
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN)
