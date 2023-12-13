c70674e8a6189705dc7ef897777bc4fda67e9f7c
Glitch (taiwa) <none>
2023-12-13 15:28:15 +0000

    👳🚈 Checkpoint ./commands/check.js:29682831/2693

--- commands/check.js
+++ commands/check.js
@@ -28,7 +28,19 @@ module.exports = {
     description: "登録された情報を表示します",
   },
   async execute(interaction) {
-    
+    let data = await db.get(interaction.guild.id)
+    if(!data){
+      data = [[undefined,undefined],[undefined,undefined],[false,undefined],false]
+      await db.set(interaction.guild.id,data)
+    }
+    const embed = new MessageEmbed()
+    .setTitle("設定状況:")
+    .addField(`= 超激通知用ch/role =`,`>>> ${data[0][0] != undefined ? "<#" + data[0][0] + ">" : "未設定"} / ${data[1][0] != undefined ? "<@&" + data[1][0] + ">" : "未設定"}`)
+    .addField(`= tohru枠通知用ch/role =`,`>>> ${data[0][1] != undefined ? "<#" + data[0][1] + ">" : "未設定"} / ${data[1][1] != undefined ? "<@&" + data[1][1] + ">" : "未設定"}`)
+    .addField(`= PET厳選機能 =`,`>>> ${data[2][0] != undefined ? data[2][0] : "未設定"} / ${data[2][1] != undefined ? data[2][1] + "%以上" : "未設定"}`)
+    .addField(`= 轢き殺し防止 =`,`>>> ${data[3] ?? "未設定"}`)
+    .setColor("RANDOM")
+    interaction.reply({ embeds: [ embed ] })
   }
 }
 
