0b1e874498a2082c5c7ad31e9edd02b9a4859058
Glitch (taiwa) <none>
2024-01-25 17:53:06 +0000

    ⛩🎌 Checkpoint ./commands/info.js:64307330/118 ./commands/check.js:64307330/33 ./commands/disable.js:64307330/25

--- commands/info.js
+++ commands/info.js
@@ -13,7 +13,7 @@ module.exports = {
   async execute(interaction) {
     const embeds = [
       new MessageEmbed()
-      .setTitle("Info:")
+      .setTitle("Info")
       .setDescription("開発者陣の情報です")
       .addField("開発者",`\`\`\`fix\n???\`\`\``)
       .addField("スペシャルサンクス",`\`\`\`fix\nanijaaa様,TAO#0042様,${interaction.user.username}様含むユーザーの皆様\`\`\``)
@@ -23,10 +23,10 @@ module.exports = {
       .setTitle("Info:")
       .setDescription("Botの情報です。")
       .addField("サーバー数",`${client.guilds.cache.size}`,true)
-      .addField("チャンネル数",`${client.channels.cache.size}\n(text:${client.channels.cache.filter(c => c.type == 'GUILD_TEXT').size},voice:1183428937846620260>:${client.channels.cache.filter(c => c.type == 'GUILD_VOICE').size},stage:${client.channels.cache.filter(c => c.type == 'GUILD_NEWS').size},<:category:1183428951125794916>:${client.channels.cache.filter(c => c.type === 'GUILD_CATEGORY').size})`,true)
-      .addField("ユーザー数",`${client.users.cache.size}\n(<:member:1183428924508733532>:${client.users.cache.filter((u) => !u.bot).size},<:bot:1183428917722349648>:${client.users.cache.filter((u) => u.bot).size})`,true)
-      .addField("開発言語","```fix\nJavaScript```",true)
-      .addField("Discord.js ver","```js\nver.13.16.0```",true)
+      .addField("チャンネル数",`${client.channels.cache.size}\ntext:${client.channels.cache.filter(c => c.type == 'GUILD_TEXT').size},voice:${client.channels.cache.filter(c => c.type == 'GUILD_VOICE').size},stage:${client.channels.cache.filter(c => c.type == 'GUILD_NEWS').size},category:${client.channels.cache.filter(c => c.type === 'GUILD_CATEGORY').size}`,true)
+      .addField("ユーザー数",`${client.users.cache.size}\n(member:${client.users.cache.filter((u) => !u.bot).size},bot:${client.users.cache.filter((u) => u.bot).size})`,true)
+      .addField("開発言語","```fix\n???```",true)
+      .addField("Discord.js ver","```js\nver.???```",true)
       .addField("Ping",`\`\`\`fix\n${client.ws.ping}ms\`\`\``,true)
       .setAuthor(`コマンド実行者:${interaction.user.tag}`, interaction.user.displayAvatarURL())
       .setColor("RANDOM")
