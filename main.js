c09ccd606e36c330e8d59e7d52320d95798bda43
kemona-331 <159854935+kemona-331@users.noreply.github.com>
2024-08-09 14:18:20 +0900

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('restart-button').addEventListener('click', function() {
-  fetch('/restart')
-    .then(response => response.text())
-    .then(data => alert(data))
-    .catch(error => console.error('Error:', error));
-});
